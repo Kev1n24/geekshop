35e6d37673d123467f57fb450fdd988c1fed1ada
Kev1n24 <96464625+Kev1n24@users.noreply.github.com>
2021-12-24 16:10:46 +0700

    Lesson 1 (#1)
    * lesson_1. Working static site

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4h747rni#l$qt_3jvb50l_dp4nzg0xaep5n#3)bzhk#yhb#pwm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
